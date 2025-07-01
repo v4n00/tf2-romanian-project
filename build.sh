54b635392026262d69f0e691300b0aac102f749c
v4n <105587619+v4n00@users.noreply.github.com>
2025-07-01 20:40:14 +0300

    soldier done

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 OUTPUT_DEST="./output/tf2-ro-pack"
 LOGO_SOURCE="./logo/new_tf2_logo.vtf"
 SOUND_SOURCE="./voicelines"
+NEW_VOICELINES_SOURCE="./tf2rop-app/dist/workdir/"
 LOGO_DEST="$OUTPUT_DEST/materials/logo"
 SOUND_DEST="$OUTPUT_DEST/sound/vo"
 VOLUME_ADJUST="3dB"
@@ -15,6 +16,12 @@ mkdir -p "$SOUND_DEST/compmode"
 mkdir -p "$OUTPUT_DEST/sound/ui"
 
 cp "$LOGO_SOURCE" "$LOGO_DEST"
+
+# voice lines
+
+find "$NEW_VOICELINES_SOURCE" -type f -name "*.mp3" | while read -r file; do
+    ffmpeg -y -i "$file" -af "volume=$VOLUME_ADJUST" "$SOUND_DEST/$(basename "$file")"
+done
 
 find "$SOUND_SOURCE" -type f -name "*.mp3" | while read -r file; do
     ffmpeg -y -i "$file" -af "volume=$VOLUME_ADJUST" "$SOUND_DEST/$(basename "$file")"
