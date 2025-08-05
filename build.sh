d85af4cd9be1f29eda597b55a4fa3dd63c0c2c24
v4n <105587619+v4n00@users.noreply.github.com>
2025-08-05 20:37:59 +0300

    upgrades

--- build.sh
+++ build.sh
@@ -15,23 +15,27 @@ INSTALL_LOCATION="${HOME}/.local/share/Steam/steamapps/common/Team Fortress 2/tf
 mkdir -p "$LOGO_DEST"
 cp "$LOGO_SOURCE" "$LOGO_DEST"
 
-# voice lines
-find "$VOICELINES_SOURCE" -type f \( -name "*.mp3" -o -name "*.wav" \) | while read -r file; do
-    target_dir="$SOUND_DEST/$(dirname "${file#$VOICELINES_SOURCE}")"
-    mkdir -p "$target_dir"
-    ffmpeg -i "$file" -af loudnorm=I=-8:LRA=18:TP=-1.5,volume=3dB -ar 44100 "$target_dir/$(basename "$file")"
-done
-
 # REMOVE AFTER REMAKE
 OLD_VOICELINES_SOURCE="./old-voicelines"
 find "$OLD_VOICELINES_SOURCE" -type f \( -name "*.mp3" -o -name "*.wav" \) | while read -r file; do
     target_dir="$SOUND_DEST/$(dirname "${file#$OLD_VOICELINES_SOURCE}")"
     mkdir -p "$target_dir"
-    ffmpeg -i "$file" -af loudnorm=I=-8:LRA=18:TP=-1.5,volume=3dB -ar 44100 "$target_dir/$(basename "$file")"
+    echo "Processing file: $file"
+    echo "Creating: $target_dir/$(basename "$file")"
+    ffmpeg -i "$file" -af loudnorm=I=-8:LRA=18:TP=-1.5,volume=3dB -ar 44100 "$target_dir/$(basename "$file")" > /dev/null 2>&1
 done
 # REMOVE AFTER REMAKE
 
+# voice lines
+find "$VOICELINES_SOURCE" -type f \( -name "*.mp3" -o -name "*.wav" \) | while read -r file; do
+    target_dir="$SOUND_DEST/$(dirname "${file#$VOICELINES_SOURCE}")"
+    mkdir -p "$target_dir"
+    echo "Processing file: $file"
+    echo "Creating: $target_dir/$(basename "$file")"
+    ffmpeg -i "$file" -af loudnorm=I=-8:LRA=18:TP=-1.5,volume=3dB -ar 44100 "$target_dir/$(basename "$file")" > /dev/null 2>&1
+done
+
 # package and install
-cd "./output" && zip -r "tf2-ro-pack.zip" "tf2-ro-pack/" && cd -
+cd "./output" && zip -r "tf2-ro-pack.zip" "tf2-ro-pack/" > /dev/null && cd -
 [[ -d "$INSTALL_LOCATION" ]] && rm -rf "$INSTALL_LOCATION/tf2-ro-pack"
 [[ -d "$INSTALL_LOCATION" ]] && cp -r "$OUTPUT_DEST" "$INSTALL_LOCATION"
--- old-voicelines/ajoin.sh
+++
@@ -1,4 +0,0 @@
-for f in Medic_*.mp3; do
-    echo "file '$f'" >> afile-list.txt
-done
-ffmpeg -f concat -safe 0 -i afile-list.txt -c copy Medic.mp3
